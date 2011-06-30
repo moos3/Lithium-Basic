ad8058f26aafccee7776f15bf147eefcde4644a2
moos3 <richard@guthnur.net>
2011-06-29 21:50:43 -0400

    Added family page

---
+++ views/pages/family.html.php
@@ -0,0 +1,6 @@
+<?php echo $this->view()->render( array('element' => 'menu'), array('posts' => false,'feeds'=>false,'family'=>true,'brewer'=>false) ); ?>
+<div id="home">
+	<div id="post">
+		<h1>Coming soon</h1>
+	</div>
+</div>
