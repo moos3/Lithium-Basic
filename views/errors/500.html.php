fe04b94393a8b375410f44f0ebbc13c259adb897
moos3 <richard@guthnur.net>
2011-06-30 08:54:08 -0400

    fixes to the error handler

--- views/errors/500.html.php
+++ views/errors/500.html.php
@@ -2,4 +2,4 @@
 	<h1>500 Error</h1>
 	<p>Something has gone wrong in the most serious way. I have gone and hired a Highly Trained team of Ninja Assassian Monkeys to locate the issue. Please give them the information below<p>
 </div>
-<?php echo sha1(md5($this->request)); ?>
+<?php echo sha1(md5($this->_request)); ?>
