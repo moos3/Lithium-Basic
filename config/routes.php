329365f303ed2cfd9262c109ef65f1856964561d
moos3 <richard@guthnur.net>
2011-06-30 08:27:37 -0400

    added comments and turned on register

--- config/routes.php
+++ config/routes.php
@@ -10,20 +10,24 @@
 use li3_rest\net\http\Router;
 use lithium\core\Environment;
 
+// Static Pages
 Router::connect('/', array('Pages::view', 'args' => array('index')));
 Router::connect('/keys', array('Pages::view', 'args' => array('keys')));
 Router::connect('/resume', array('Pages::view', 'args' => array('resume')));
 Router::connect('/social',array('Pages::view','args' => array('feeds')));
 Router::connect('/family',array('Pages::view','args' => array('family')));
 
+// User Management
 Router::connect('/login', array('Users::login'));
 Router::connect('/logout', array('Users::logout'));
-//Router::connect('/register', array('Users::register'));
+Router::connect('/register', array('Users::register'));
+
 Router::connect('/pages/{:args}', 'Pages::view');
 if (!Environment::is('production')) {
 	Router::connect('/test/{:args}', array('controller' => 'lithium\test\Controller'));
 	Router::connect('/test', array('controller' => 'lithium\test\Controller'));
 }
+// Blog
 Router::resource('Posts');
 Router::connect('/posts/{:id:[0-9a-f]{24}}.{:type}', array('action' => 'view', 'id' => null));
 Router::connect('/posts/{:slug:[a-z0-9-]+}.{:type}', array('Posts::view'));
